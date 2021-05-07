03cbf8067947781e29ebaa7fcd03a82f55d65407
wonderboixixo <64201620+wonderboixixo@users.noreply.github.com>
2021-05-07 12:06:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@
 wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
 unzip -q rclone-v1.52.3-linux-amd64.zip
 export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
+#Install fclone static binary
+wget -q https://github.com/mawaya/rclone/releases/download/fclone-v0.4.1/fclone-v0.4.1-linux-amd64.zip
+unzip -q fclone-v0.4.1-linux-amd64.zip
+export PATH=$PWD/fclone-v0.4.1-linux-amd64:$PATH
